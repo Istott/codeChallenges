4fb4192dab1f61a2ad8ad84bb69d6c861c4fe002
istott <isaac.cloyd@gmail.com>
2020-10-27 17:53:56 -0600

    first commit of a folder I have solving code challenges in

---
+++ miniMaxSum.py
@@ -0,0 +1,42 @@
+def miniMaxSum(arr):
+    table = {
+        'cur': 0, 
+        'tallymax': 0, 
+        'tallymin': 0,
+        'max': max(arr),
+        'min': min(arr)
+    }
+
+    if table['max'] == table['min']:
+        table['tallymax'] = table['max'] * 4
+        table['tallymin'] = table['min'] * 4
+
+    for x in range(len(arr)):
+        if table['cur'] != 0:
+            if arr[x] != table['max']:
+                table['tallymin'] += arr[x]
+                table['cur'] = arr[x]
+        else:
+            if table['max'] != arr[x]:
+                table['cur'] = arr[x]
+                table['tallymin'] = arr[x]
+        
+    table['cur'] = 0
+
+    for x in range(len(arr)):
+        if table['cur'] != 0:
+            if arr[x] != table['min']:
+                table['tallymax'] += arr[x]
+                table['cur'] = arr[x]
+        else:
+            if table['min'] != arr[x]:
+                table['cur'] = arr[x]
+                table['tallymax'] = arr[x]
+
+
+    print(table['tallymin'], table['tallymax'])
+
+
+newArr = [5,5,5,5,5]
+
+print(miniMaxSum(newArr))
---
+++ stringAnagram.py
@@ -0,0 +1,40 @@
+def stringAnagram(dictionary, query):
+    dictTab = {}
+    queryTab = {}
+    result = []
+
+    for x in range(len(dictionary)):
+            dictTab[x] = {}
+            for j in dictionary[x]:
+                if j in dictTab[x]:
+                    dictTab[x][j] += 1
+                else:
+                    dictTab[x][j] = 1
+    
+    for x in range(len(query)):
+            queryTab[x] = {}
+            for j in query[x]:
+                if j in queryTab[x]:
+                    queryTab[x][j] += 1
+                else:
+                    queryTab[x][j] = 1
+
+    for x in queryTab:
+        counter = 0
+        for j in dictTab:
+            if queryTab[x] == dictTab[j]:
+                counter += 1
+            
+        result.append(counter)
+
+    return result
+    # print(dictTab)
+    # print(queryTab)
+
+
+
+
+q = ['a', 'nark', 'bs', 'hack', 'stair']
+dic = ['hack', 'a', 'rank', 'khac', 'ackh', 'kran', 'rankhacker', 'a', 'ab', 'ba', 'stairs', 'raits']
+
+print(stringAnagram(dic, q))
