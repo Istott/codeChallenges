53aaa76f7db8c6f3acae044ab3373575e37b215c
Isaac Stott <IStott@midwayusa.com>
2021-09-15 16:42:09 -0500

    solved all edges cases for repeated-string

--- repeated-string.py
+++ repeated-string.py
@@ -1,17 +1,22 @@
 
 
 def repeatedString(s, n):
-    if len(s) == 1:
+    if len(s) == 1 and s == 'a':
         return n
 
     newStr = s
     count = 0
+    loopCounter = 0
     divCount = n // len(s)
     moduloCount = n % len(s)
 
     for x in s:
+        loopCounter += 1
         if x == 'a':
             count += 1
+        
+        if loopCounter == n:
+            break
     
     if divCount >= 1:
         count = count * divCount
